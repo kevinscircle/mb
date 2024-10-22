c2ba9407bd710340bb7e1e43495494a2556bf0fb
kevinscircle@aol.com <kevingeoreyes@gmail.com>
2024-10-21 21:53:07 -0700

    first commit

---
+++ posts/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Post(models.Model):                                   # inheritance
+    title = models.CharField(max_length=128)                # composition 
+    subtitle = models.CharField(max_length=256)             # composition 
+    body = models.TextField()                               # composition 
+    created_on = models.DateTimeField(auto_now_add=True)    # composition 
